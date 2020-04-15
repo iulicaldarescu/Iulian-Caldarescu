7da33b256517291e23d46268e865adbd4d741bae
Iuli <caldarescuiulian@gmail.com>
2020-04-15 19:38:15 +0300

    Basic Code Challenge Stage 1

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -2,6 +2,14 @@ package lab1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("dassda");
+        //FizzBuzz
+        for (int i = 1; i <= 100; i++){
+            if(i % 3 == 0){
+                System.out.print("Fizz, ");
+            }else if(i % 5 == 0){
+                System.out.print("Buzz, ");
+            }else
+                System.out.print(i + ", ");
+        }
    }
 }
