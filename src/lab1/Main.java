e56dcba0d2564decb4564c61fb088e974241e3aa
Iuli <caldarescuiulian@gmail.com>
2020-04-15 20:27:14 +0300

    added Stage 2

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -8,6 +8,10 @@ public class Main {
                 System.out.print("Fizz, ");
             }else if(i % 5 == 0){
                 System.out.print("Buzz, ");
+            }else if(i % 7 == 0){
+                System.out.print("Rizz, ");
+            }else if(i % 11 == 0){
+                System.out.print("Jazz, ");
             }else
                 System.out.print(i + ", ");
         }
